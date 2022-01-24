68b1d10e8b65fcc7f4228f73443ddfc3da31abc0
v.yakovlev <difencee@gmail.com>
2022-01-24 15:44:25 +0300

    my changes

--- src/MA/text.java
+++ src/MA/text.java
@@ -6,10 +6,5 @@ public class text {
         System.out.println("cya");
         System.out.println("2");
         System.out.println("3");
-
-
-        System.out.println("2nd");
-        System.out.println("2nd");
-        System.out.println("3nd");
     }
 }
