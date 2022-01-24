b8302fe82ca0d1de687b2e1c240f9d7be64cf3ea
v.yakovlev <difencee@gmail.com>
2022-01-24 15:35:20 +0300

    1

--- src/MA/text.java
+++ src/MA/text.java
@@ -2,6 +2,7 @@ package MA;
 
 public class text {
     public static void main(String[] args) {
-        System.out.println("hello1");
+        System.out.println("hello");
+        System.out.println("cya");
     }
 }
