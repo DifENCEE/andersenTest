99f9c08fef03c026746bd804ad4e5e6da6d8aacd
v.yakovlev <difencee@gmail.com>
2022-01-24 15:39:00 +0300

    11

--- src/MA/text.java
+++ src/MA/text.java
@@ -4,5 +4,6 @@ public class text {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("cya");
+        System.out.println("1");
     }
 }
