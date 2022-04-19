a8d5d333472a1b355b17e144dad89834577ac968
v.yakovlev <difencee@gmail.com>
2022-04-19 12:03:51 +0300

    hii

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -2,6 +2,8 @@ package MA;
 
 public class Text {
     public static void main(String[] args) {
+        System.out.println("jjj");
+
         System.out.println("1");
         System.out.println("sssse");
         System.out.println("hii");
