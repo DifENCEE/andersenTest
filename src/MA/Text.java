047b3365299e4069f5817cae3fc20b3769e77acc
v.yakovlev <difencee@gmail.com>
2022-04-19 12:09:57 +0300

    aaa

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -2,6 +2,7 @@ package MA;
 
 public class Text {
     public static void main(String[] args) {
+        System.out.println("mmm");
 
         System.out.println("sssse");
         System.out.println("hii");
