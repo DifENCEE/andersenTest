23985ce19f4b21a9369f5e65291ba36af7f253b7
v.yakovlev <difencee@gmail.com>
2022-04-19 12:18:16 +0300

    aaa

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -2,13 +2,7 @@ package MA;
 
 public class Text {
     public static void main(String[] args) {
-        System.out.println("mmm");
-
-        System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("sssse");
-        System.out.println("hii");
-        System.out.println("hii");
+        System.out.println("delete all");
 
     }
 }
