c44a3c989b0eb53e16402b8fd198d657961a1716
v.yakovlev <difencee@gmail.com>
2022-04-19 12:03:23 +0300

    hii

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -2,24 +2,6 @@ package MA;
 
 public class Text {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("cya");
-        System.out.println("2");
-        System.out.println("3");
-
-        System.out.println("q");
-        System.out.println("q");
-        System.out.println("qq");
-
-        System.out.println("bye");
-        System.out.println("bye");
-
-
-        System.out.println("uu");
-        System.out.println("aaa");
-        System.out.println("ss");
-        System.out.println("buu");
-
         System.out.println("1");
         System.out.println("sssse");
         System.out.println("hii");
