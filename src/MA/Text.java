174e929c8844ae94ed6a07fe4afaff555fc53172
v.yakovlev <difencee@gmail.com>
2022-04-19 12:23:15 +0300

    aaa

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -4,6 +4,7 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("delete all");
         System.out.println("test");
+        System.out.println("new!");
 
     }
 }
