a3b9c71e51f78fbf52c2e43be9cf0ff9a7651146
v.yakovlev <difencee@gmail.com>
2022-04-22 17:20:53 +0300

    123

--- src/main/java/MA/Text.java
+++ src/main/java/MA/Text.java
@@ -3,8 +3,6 @@ package MA;
 public class Text {
     public static void main(String[] args) {
         System.out.println("change from dev");
-        System.out.println("change from dev1");
-        System.out.println("change from dev2");
 
 
     }
