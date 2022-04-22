721012eb2b652de10dc9fee1329750e2f4b97b9d
v.yakovlev <difencee@gmail.com>
2022-04-22 16:02:52 +0300

    123

--- src/MA/Text.java
+++ src/MA/Text.java
@@ -2,13 +2,7 @@ package MA;
 
 public class Text {
     public static void main(String[] args) {
-        System.out.println("dev branch changes");
-        System.out.println("dev branch changes1");
 
-        System.out.println("the world is mine");
-        System.out.println("mergeTest");
-        System.out.println("mergeTest2");
-        System.out.println("mergeTest3");
 
     }
 }
