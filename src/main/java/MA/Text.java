7117c55c946060c8c788fc57e15dec07130838cc
v.yakovlev <difencee@gmail.com>
2022-04-22 17:33:15 +0300

    123

--- src/main/java/MA/Text.java
+++ src/main/java/MA/Text.java
@@ -4,6 +4,7 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("change from dev");
         System.out.println("change from dev1");
+        System.out.println("change from dev2");
 
 
     }
