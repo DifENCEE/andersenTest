3772a91455fc3fea1deb4d80638c773bf5f87fc6
v.yakovlev <difencee@gmail.com>
2022-06-30 14:09:51 +0300

    test

--- src/main/java/MA/Text.java
+++ src/main/java/MA/Text.java
@@ -6,10 +6,8 @@ public class Text {
         System.out.println("change from dev1");
         System.out.println("change from dev2");
 
-        System.out.println("branch changes");
-        System.out.println("branch changes1");
-        System.out.println("branch changes2");
-        System.out.println("branch changes3");
-        System.out.println("branch changes4");
+
+
+        System.out.println("new changes here");
     }
 }
