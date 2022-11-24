e0c3711d444a5771262b76df012af80cedcf813c
v.yakovlev <difencee@gmail.com>
2022-11-24 17:38:21 +0300

    rebse new test

--- simple-module/src/main/java/ru/vladi/experiments/TrashHolderOnlyForExperiments.java
+++ simple-module/src/main/java/ru/vladi/experiments/TrashHolderOnlyForExperiments.java
@@ -23,6 +23,8 @@ public class TrashHolderOnlyForExperiments {
         System.out.println("here is a main changes");
 
         System.out.println("stupid test");
+        System.out.println("stupid test");
+        System.out.println("stupid test");
 
         //imported from dependency
         FourthTask.main(args);
