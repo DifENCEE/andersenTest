0f065f75e818f0990b79b1f35ddb8cee152f69a0
Vladislav Yakovlev <difencee@gmail.com>
2023-10-19 23:00:40 +0500

    changes after squash11

--- simple-module/src/main/java/ru/vladi/experiments/TrashHolderOnlyForExperiments.java
+++ simple-module/src/main/java/ru/vladi/experiments/TrashHolderOnlyForExperiments.java
@@ -27,6 +27,7 @@ public class TrashHolderOnlyForExperiments {
 
         System.out.println("after squash cherry pick test");
         System.out.println("after squash cherry pick changes");
+        System.out.println("after squash cherry pick changes1");
 
         //imported from dependency
         FourthTask.main(args);
